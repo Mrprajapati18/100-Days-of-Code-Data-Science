84c2f38733f92421b5ed9f93e841435063228874
Durgesh kumar prajapati <durgeshkumar778080@gmail.com>
2025-03-22 01:04:58 +0530

    Data Structure And Algorithms

---
+++ Data Structrure & Algorithms/Set.py
@@ -0,0 +1,21 @@
+# list
+# lst = [40, 40, 50, 50, 90, 90]
+# print(lst)
+
+# #tuple
+# tup = (40, 40, 50, 50, 90, 90)
+# print(tup)
+
+# #Set is not allow the duplicate in 
+# set = {40, 40, 50, 50, 90, 90}
+# print(set)
+
+lst = [1, 2, 2, 3, 3, 3]
+print(lst)
+
+tup = (1, 2, 2, 3, 3, 3)
+print(tup)
+
+S1 = {1, 2, 2, 3, 3, 3}
+print(S1)
+
---
+++ Data Structrure & Algorithms/unpacking&unpacking.py
@@ -0,0 +1,9 @@
+# Packing
+tup = (10, 30, 40)
+
+# Unpacking
+num1, num2, num3 = tup
+
+print(f"Num1 = {num1}")
+print(f"Num2 = {num2}")
+print(f"Num3 = {num3}")
